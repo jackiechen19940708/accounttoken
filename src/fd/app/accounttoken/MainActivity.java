9e8164c2fb5c3324f39653df0bc92da0d2c625e2
Jackie Chen <12307130250@fudan.edu.cn>
2015-11-24 12:21:07 +0800

    ProgressBar View added

--- src/fd/app/accounttoken/MainActivity.java
+++ src/fd/app/accounttoken/MainActivity.java
@@ -2,8 +2,14 @@ package fd.app.accounttoken;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import fd.app.myview.RoundProgressBar;
 
 public class MainActivity extends Activity {
+    private RoundProgressBar roundProgressBar=null;
+    private Button start=null;
+    int  i=0;
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +17,19 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        roundProgressBar= (RoundProgressBar) findViewById(R.id.roundProgressBar2);
+        start= (Button) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        i=(i+1)%100;
+                        roundProgressBar.setProgress(i);
+                    }
+                }).start();
+                            }
+        });
     }
 }
